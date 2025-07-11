4901eaf16897f272b8f72a0cbc881d9687853e8a
BhavaniSankar-Somaraju <bhavanisankar136@gmail.com>
2025-07-11 17:17:44 +0530

    Feat1: Learn the Basics in DSA

--- Step1_Learn_The_Basics/PassByValueEx2.java
+++ Step1_Learn_The_Basics/PassByValueEx2.java
@@ -7,7 +7,7 @@ public class PassByValueEx2 {
         int[] a= {10,20,30};
          System.out.println("array value of a is"+Arrays.toString(a));
         givevalue(a);
-        System.out.println("array value of a is"+Arrays.toString(a));
+        System.out.println("array value of a is"+Arrays.toString(a));//here for objects the copy of value of the reference is passed so the array is changing here.
        
 
     }
